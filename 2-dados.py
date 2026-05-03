e56ad80f2dac2aa4e61443beb5403f7b2179db05
Yuri Zurkovisk <yurizurkovisk@gmail.com>
2026-05-03 20:28:34 -0300

    Primeiro curso de Python

---
+++ 2-dados.py
@@ -0,0 +1,14 @@
+# Pythonflix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+noteMovie = 9.5
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(noteMovie)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(planIncluded))
